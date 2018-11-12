c8b2dd0f1ba44ef11e9311b71975a778d407e015
Vibha-Gupta <30341845+Vibha-Gupta@users.noreply.github.com>
2018-11-12 16:40:40 +0530

    Add files via upload

---
+++ test/apptest.js
@@ -0,0 +1,16 @@
+'use strict';
+const assert = require('chai').assert
+const app = require('../app');
+let name='asssss';
+//var name1 = 'xyz';
+
+describe('App', function(){
+it('app should return hello', function(){
+    let name = 'sd'
+    console.log(name);   
+
+});
+ console.log(name);
+});
+
+
